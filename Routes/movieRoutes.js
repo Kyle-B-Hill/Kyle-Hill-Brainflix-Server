5a0b35a2d946c1153ef4f2a5b73ce444cfb7dd7e
Kyle Hill <kbhill@lakeheadu.ca>
2024-01-14 00:30:09 -0500

    post request built out

--- Routes/movieRoutes.js
+++ Routes/movieRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 router.get("/",(request, response)=>{
     const moviesJSON = fs.readFileSync("./Data/movies.json");
@@ -33,6 +34,33 @@ router.get("/:movieId",(request, response)=>{
     }
     
 });
+
+const movieDefaults = {
+    id: "This is a movie Id",
+    title: "This is a movie title",
+    channel: "User Upload",
+    image: "/static/StaticImage.jpg",
+    description: "This is a movie description",
+    views: "0",
+    likes: "0",
+    duration: "9:00",
+    video: "https://project-2-api.herokuapp.com/stream",
+    timestamp: "This is a movie timestamp",
+    comments: [],
+}
+
+    // title, description, we add timestamp, id 
+
+router.post("/",(request, response)=>{
+    const movie = { ...movieDefaults, ...request.body, timestamp:Date.now(), id:uuidv4()}
+    // console.log(request.body);
+    const moviesJSON = fs.readFileSync("./data/movies.json");
+    const movies = JSON.parse(moviesJSON);
+    movies.push(movie);
+    fs.writeFileSync("./data/movies.json", JSON.stringify(movies));
+    response.status(201).json(movie);
+
+})
 
 
 
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const movieRouter = require("./Routes/movieRoutes.js");
 
-// app.use(express.json());
+app.use(express.json());
 
 app.use("/movies", movieRouter);
+
+app.use("/static",express.static("./static"));
 
 app.listen(8080, () => {
     console.log("Server is running on port 8080");
