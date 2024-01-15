f95d0cd851062993f5cd03a17476475f99d6fed0
Kyle Hill <kbhill@lakeheadu.ca>
2024-01-15 17:22:45 -0500

    removed added test videos

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const movieRouter = require("./Routes/movieRoutes.js");
 const cors = require("cors");
 require("dotenv").config();
 
-// app.use(cors());
-
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
@@ -16,10 +14,6 @@ app.use(cors({ origin: CORS_ORIGIN }));
 app.use("/movies", movieRouter);
 
 app.use("/public/images",express.static("./public/images"));
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
