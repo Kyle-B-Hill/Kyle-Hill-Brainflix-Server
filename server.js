af44c1b084d59e4504f27c2719513b9bd78eac6d
Kyle Hill <kbhill@lakeheadu.ca>
2024-01-15 01:24:04 -0500

    added env

--- server.js
+++ server.js
@@ -2,17 +2,25 @@ const express = require("express");
 const app = express();
 const movieRouter = require("./Routes/movieRoutes.js");
 const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3000";
+
+app.use(cors({ origin: CORS_ORIGIN }));
 
 app.use("/movies", movieRouter);
 
 app.use("/public/images",express.static("./public/images"));
 
-app.listen(8080, () => {
-    console.log("Server is running on port 8080");
-})
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// })
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 
